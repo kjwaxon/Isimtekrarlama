fed64ea6d6c0b3aaff87816d95458b4c3eca73f7
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:39:57 +0300

    Consoleappisimtekrarlama

---
+++ ConsoleAppİsim/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppİsim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+
+            int n;
+
+            Console.Write("Tekrarlamak istediğiniz mesajı yazınız = ");
+
+            x = Console.ReadLine();
+
+            Console.Write("Kaç kere tekrarlamak istediğinizi yazınız = ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
